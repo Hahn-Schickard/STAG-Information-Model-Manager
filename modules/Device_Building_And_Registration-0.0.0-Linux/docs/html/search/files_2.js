42022f210237f3821e19a64ce4f601570d648f57
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-09-26 18:08:59 +0200

    added Device BUilding and Registration interface dependency
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>

---
+++ modules/Device_Building_And_Registration-0.0.0-Linux/docs/html/search/files_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['doc_5fmain_5fpage_2emd',['doc_main_page.md',['../d4/dce/docs_2code__documentation_2doc__main__page_8md.html',1,'']]],
+  ['doxygen_5fspecial_5fcommand_5fcheatsheet_2emd',['doxygen_special_command_cheatsheet.md',['../d7/de1/docs_2code__documentation_2doxygen__special__command__cheatsheet_8md.html',1,'']]]
+];
---
+++ modules/Device_Building_And_Registration-0.0.0-Linux/docs/html/search/pages_1.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['doxygen_20special_20command_20cheatsheet',['Doxygen special command cheatsheet',['../df/d1c/md_build__CPack_Packages_Linux_TGZ_Device_Building_And_Registration-0_80_80-Linux_docs_doxygen_special_command_cheatsheet.html',1,'']]],
+  ['doxygen_20special_20command_20cheatsheet',['Doxygen special command cheatsheet',['../dd/d5b/md_build__CPack_Packages_Linux_TGZ_Device_Building_And_Registration-0_80_80-Linux_modules_Informbd2e067a3f50a459a2fa7bbe0dce148c.html',1,'']]],
+  ['doxygen_20special_20command_20cheatsheet',['Doxygen special command cheatsheet',['../d0/dfd/md_docs_code_documentation_doxygen_special_command_cheatsheet.html',1,'']]],
+  ['doxygen_20special_20command_20cheatsheet',['Doxygen special command cheatsheet',['../d4/d54/md_modules_Information_Modeler-0_80_80-Linux_docs_doxygen_special_command_cheatsheet.html',1,'']]],
+  ['device_20building_5fand_5fregistration',['Device Building_And_Registration',['../d0/d30/md_README.html',1,'']]]
+];
