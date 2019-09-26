2c07289066e309192190269b70666d829bf3a5cb
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2019-09-26 18:13:24 +0200

    updated event handling

--- sources/Model_Manager/ModelManager.cpp
+++ sources/Model_Manager/ModelManager.cpp
@@ -1,14 +1,17 @@
 #include "ModelManager.hpp"
+#include "Notifier_Event.hpp"
 
 using namespace std;
 using namespace Model_Manager;
+using namespace Model_Event_Handler;
 
 void ModelManager::registerDevice(
-    std::unique_ptr<Information_Model::Device> device)
-{
+    std::unique_ptr<Information_Model::Device> device) {
   pair<string, Information_Model::Device *> device_pair(
       device->getElementRefId(),
       device.get()); //@TODO: check if ths causes problems
   devices.insert(device_pair);
-  notifyListeners(device.get());
+  NotifierEvent *event =
+      new NotifierEvent(NotifierEventType::NEW_DEVICE_REGISTERED, device.get());
+  notifyListeners(event);
 }
