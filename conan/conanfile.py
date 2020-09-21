0fe7595453237bf0fbe77c7ca63b5fd919522937
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-09-21 18:47:47 +0200

    fix conan package integration test

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -8,7 +8,7 @@ class PackageConan(ConanFile):
     license = "Apache 2.0"
     topics = ("conan", "stag", "modelling")
     build_requires = "gtest/1.10.0"
-    requires = ""
+    requires = "HaSLL/0.2.2@hahn-schickard/stable", "Information_Model/0.1.0@hahn-schickard/stable", "Model_Event_Handler/0.1.0@hahn-schickard/stable", "Device_Builder/0.1.0@hahn-schickard/stable", "Device_Building_And_Registration/0.1.0@hahn-schickard/stable", "Technology_Interface/0.1.0@hahn-schickard/stable"
     settings = "cppstd", "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False],
                "fPIC": [True, False]}
