4f79a95d2ee7537799452dbe0dd67598da56ab0f
Mark Weyer <Mark.Weyer@hahn-schickard.de>
2022-02-02 14:24:36 +0100

    Bump dependencies of Data_Consumer_Adapter_Interface and Technology_Adapter_Interface

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -11,8 +11,8 @@ class PackageConan(ConanFile):
     build_requires = "gtest/1.10.0"
     requires = [
         "HaSLL/0.2.2@hahn-schickard/stable",
-        "Data_Consumer_Adapter_Interface/0.1.7@hahn-schickard/experiment_7.0",
-        "Technology_Adapter_Interface/0.1.6@hahn-schickard/experiment_11.0"
+        "Data_Consumer_Adapter_Interface/0.1.9@hahn-schickard/stable",
+        "Technology_Adapter_Interface/0.1.8@hahn-schickard/stable"
     ]
     settings = "cppstd", "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False],
