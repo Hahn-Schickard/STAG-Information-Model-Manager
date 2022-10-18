38e70d3254c02ab4eeb4144c22c3fd34d0e3679f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-10-18 14:08:33 +0200

    refactor for HaSLL v0.3.1

--- sources/Model_Registry/ModelRegistry.cpp
+++ sources/Model_Registry/ModelRegistry.cpp
@@ -1,31 +1,27 @@
 #include "ModelRegistry.hpp"
-#include "LoggerRepository.hpp"
+#include "HaSLL/LoggerManager.hpp"
 
 #include <functional>
 
 using namespace std;
 using namespace Information_Model;
 using namespace DCAI;
-using namespace HaSLL;
+using namespace HaSLI;
 
 namespace Information_Model_Manager {
 ModelRegistry::ModelRegistry()
     : AsyncEventSource(
           bind(&ModelRegistry::logException, this, placeholders::_1)),
-      logger_(LoggerRepository::getInstance().registerTypedLoger(this)) {}
+      logger_(LoggerManager::registerTypedLogger(this)) {}
 
-ModelRegistry::~ModelRegistry() {
-  logger_->flushMessages();
-  LoggerRepository::getInstance().deregisterLoger(logger_->getName());
-}
+ModelRegistry::~ModelRegistry() { logger_->flushMessages(); }
 
 void ModelRegistry::logException(std::exception_ptr ex_ptr) {
   try {
     if (ex_ptr)
       rethrow_exception(ex_ptr);
-  } catch (exception &ex) {
-    logger_->log(
-        SeverityLevel::ERROR,
+  } catch (exception& ex) {
+    logger_->log(SeverityLevel::ERROR,
         "Received an exception while trying to notify listeners. Exception: {}",
         ex.what());
   }
@@ -34,39 +30,38 @@ void ModelRegistry::logException(std::exception_ptr ex_ptr) {
 bool ModelRegistry::registerDevice(shared_ptr<Device> device) {
   if (!deviceExists(device->getElementId())) {
     logger_->log(SeverityLevel::TRACE,
-                 "Registering device with id: {} within the Information Model",
-                 device->getElementId());
-    pair<string, shared_ptr<Device>> device_pair(device->getElementId(),
-                                                 device);
+        "Registering device with id: {} within the Information Model",
+        device->getElementId());
+    pair<string, shared_ptr<Device>> device_pair(
+        device->getElementId(), device);
     devices_.insert(device_pair);
     notify(make_shared<ModelRegistryEvent>(device));
     return true;
   } else {
     logger_->log(SeverityLevel::TRACE, "Device with id {} already exists!",
-                 device->getElementId());
+        device->getElementId());
     return false;
   }
 }
 
-bool ModelRegistry::deregisterDevice(const string &device_id) {
+bool ModelRegistry::deregisterDevice(const string& device_id) {
   if (deviceExists(device_id)) {
     logger_->log(SeverityLevel::TRACE,
-                 "Removing Device with id {} from the Information Model",
-                 device_id);
+        "Removing Device with id {} from the Information Model", device_id);
     notify(make_shared<ModelRegistryEvent>(device_id));
     devices_.erase(device_id);
     return true;
   } else {
-    logger_->log(SeverityLevel::TRACE, "Device with id {} does not exist!",
-                 device_id);
+    logger_->log(
+        SeverityLevel::TRACE, "Device with id {} does not exist!", device_id);
     return false;
   }
 }
 
-bool ModelRegistry::deviceExists(const string &device_id) {
+bool ModelRegistry::deviceExists(const string& device_id) {
   if (devices_.find(device_id) == devices_.end()) {
-    logger_->log(SeverityLevel::TRACE, "Device with id {} not found!",
-                 device_id);
+    logger_->log(
+        SeverityLevel::TRACE, "Device with id {} not found!", device_id);
     return false;
   } else {
     return true;
--- unit_tests/testRunner.cpp
+++ unit_tests/testRunner.cpp
@@ -1,12 +1,15 @@
-#include "LoggerRepository.hpp"
+#include "HaSLL/LoggerManager.hpp"
+#include "HaSLL/SPD_LoggerRepository.hpp"
 
 #include "gtest/gtest.h"
 
-int main(int argc, char **argv) {
-  auto config = HaSLL::Configuration(
-      "./log", "logfile.log", "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
-      HaSLL::SeverityLevel::TRACE, false, 8192, 2, 25, 100, 1);
-  HaSLL::LoggerRepository::initialise(config);
+using namespace HaSLL;
+using namespace std;
+
+int main(int argc, char** argv) {
+  auto repo = std::make_shared<SPD_LoggerRepository>();
+  LoggerManager::initialise(repo);
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
