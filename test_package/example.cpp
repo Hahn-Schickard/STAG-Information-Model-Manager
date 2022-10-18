04d4b6d749d9c72dd95771d32b14c223452a2992
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-10-18 14:08:07 +0200

    apply project template v0.1.12 changes

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,5 +1,6 @@
+#include "HaSLL/LoggerManager.hpp"
+#include "HaSLL/SPD_LoggerRepository.hpp"
 #include "Information_Model_Manager/ModelManager.hpp"
-#include "LoggerRepository.hpp"
 
 #include <stdexcept>
 
@@ -9,15 +10,13 @@ using namespace Information_Model_Manager;
 
 int main() {
   try {
-    auto config = Configuration(
-        "./log", "logfile.log", "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
-        SeverityLevel::TRACE, false, 8192, 2, 25, 100, 1);
-    LoggerRepository::initialise(config);
+    auto repo = std::make_shared<SPD_LoggerRepository>();
+    LoggerManager::initialise(repo);
 
     auto technology_manager = ModelManager();
     if (!technology_manager.getModelEventSource())
       throw runtime_error("Event source can not be null ptr");
-  } catch (const exception &ex) {
+  } catch (const exception& ex) {
     exit(EXIT_FAILURE);
   }
 
