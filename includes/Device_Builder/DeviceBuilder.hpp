5b84cd00f224b9015ebc3892a225076071408206
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-10-18 14:32:42 +0200

    add missing optional includes

--- includes/Device_Builder/DeviceBuilder.hpp
+++ includes/Device_Builder/DeviceBuilder.hpp
@@ -4,48 +4,45 @@
 #include "DeviceImplementationBuilder.hpp"
 #include "Information_Model/DeviceBuilderInterface.hpp"
 
+#include <optional>
+
 namespace Information_Model_Manager {
 class DeviceBuilder : public Information_Model::DeviceBuilderInterface {
   std::unique_ptr<DeviceImplementationBuilder> builder_;
 
 public:
-  void buildDeviceBase(const std::string &unique_id, const std::string &name,
-                       const std::string &desc) override;
+  void buildDeviceBase(const std::string& unique_id, const std::string& name,
+      const std::string& desc) override;
 
-  std::string addDeviceElementGroup(const std::string &name,
-                                    const std::string &desc) override;
+  std::string addDeviceElementGroup(
+      const std::string& name, const std::string& desc) override;
 
-  std::string addDeviceElementGroup(const std::string &group_refid,
-                                    const std::string &name,
-                                    const std::string &desc) override;
+  std::string addDeviceElementGroup(const std::string& group_refid,
+      const std::string& name, const std::string& desc) override;
 
-  std::string
-  addReadableMetric(const std::string &name, const std::string &desc,
-                    Information_Model::DataType data_type,
-                    Information_Model::ReadFunctor read_cb) override;
+  std::string addReadableMetric(const std::string& name,
+      const std::string& desc, Information_Model::DataType data_type,
+      Information_Model::ReadFunctor read_cb) override;
 
-  std::string
-  addReadableMetric(const std::string &group_refid, const std::string &name,
-                    const std::string &desc,
-                    Information_Model::DataType data_type,
-                    Information_Model::ReadFunctor read_cb) override;
+  std::string addReadableMetric(const std::string& group_refid,
+      const std::string& name, const std::string& desc,
+      Information_Model::DataType data_type,
+      Information_Model::ReadFunctor read_cb) override;
 
-  std::string
-  addWritableMetric(const std::string &name, const std::string &desc,
-                    Information_Model::DataType data_type,
-                    std::optional<Information_Model::ReadFunctor> read_cb,
-                    Information_Model::WriteFunctor write_cb) override;
+  std::string addWritableMetric(const std::string& name,
+      const std::string& desc, Information_Model::DataType data_type,
+      std::optional<Information_Model::ReadFunctor> read_cb,
+      Information_Model::WriteFunctor write_cb) override;
 
-  std::string
-  addWritableMetric(const std::string &group_refid, const std::string &name,
-                    const std::string &desc,
-                    Information_Model::DataType data_type,
-                    std::optional<Information_Model::ReadFunctor> read_cb,
-                    Information_Model::WriteFunctor write_cb) override;
+  std::string addWritableMetric(const std::string& group_refid,
+      const std::string& name, const std::string& desc,
+      Information_Model::DataType data_type,
+      std::optional<Information_Model::ReadFunctor> read_cb,
+      Information_Model::WriteFunctor write_cb) override;
 
-  std::string addDeviceElement(
-      const std::string &group_refid, const std::string &name,
-      const std::string &desc, Information_Model::ElementType type,
+  std::string addDeviceElement(const std::string& group_refid,
+      const std::string& name, const std::string& desc,
+      Information_Model::ElementType type,
       Information_Model::DataType data_type,
       std::optional<Information_Model::ReadFunctor> read_cb,
       std::optional<Information_Model::WriteFunctor> write_cb) override;
--- includes/Device_Builder/DeviceElementGroupImplementation.hpp
+++ includes/Device_Builder/DeviceElementGroupImplementation.hpp
@@ -1,21 +1,22 @@
 #ifndef __MODEL_BUILDER_DEVICE_ELEMENT_GROUP_IMPLEMENTATION_HPP_
 #define __MODEL_BUILDER_DEVICE_ELEMENT_GROUP_IMPLEMENTATION_HPP_
 
-#include "Information_Model/DeviceElementGroup.hpp"
 #include "Information_Model/DeviceElement.hpp"
+#include "Information_Model/DeviceElementGroup.hpp"
 #include "MetricImplementation.hpp"
 #include "WritableMetricImplementation.hpp"
 
+#include <optional>
 #include <unordered_map>
 
 namespace Information_Model_Manager {
 class DeviceElementGroupImplementation
     : public Information_Model::DeviceElementGroup {
-  using DeviceElementsMap =
-      std::unordered_map<std::string,
-                         Information_Model::NonemptyDeviceElementPtr>;
+  using DeviceElementsMap = std::unordered_map<std::string,
+      Information_Model::NonemptyDeviceElementPtr>;
   using SubgroupsMap = std::unordered_map<std::string,
-    NonemptyPointer::NonemptyPtr<std::shared_ptr<DeviceElementGroupImplementation>>>;
+      NonemptyPointer::NonemptyPtr<
+          std::shared_ptr<DeviceElementGroupImplementation>>>;
 
   DeviceElementsMap elements_map_;
   SubgroupsMap subgroups_map_;
@@ -25,26 +26,24 @@ class DeviceElementGroupImplementation
   std::string generate_Reference_ID();
 
 public:
-  DeviceElementGroupImplementation(const std::string &base_ref_id);
+  DeviceElementGroupImplementation(const std::string& base_ref_id);
 
   std::vector<Information_Model::NonemptyDeviceElementPtr> getSubelements();
 
-  std::shared_ptr<Information_Model::DeviceElement>
-  getSubelement(const std::string &ref_id);
+  std::shared_ptr<Information_Model::DeviceElement> getSubelement(
+      const std::string& ref_id);
 
   std::shared_ptr<DeviceElementGroupImplementation> getSubgroupImplementation(
-    const std::string & ref_id);
+      const std::string& ref_id);
 
-  std::string addSubgroup(const std::string &name, const std::string &desc);
+  std::string addSubgroup(const std::string& name, const std::string& desc);
 
-  std::string
-  addReadableMetric(const std::string &name, const std::string &desc,
-                    Information_Model::DataType data_type,
-                    std::function<Information_Model::DataVariant()> read_cb);
+  std::string addReadableMetric(const std::string& name,
+      const std::string& desc, Information_Model::DataType data_type,
+      std::function<Information_Model::DataVariant()> read_cb);
 
-  std::string addWritableMetric(
-      const std::string &name, const std::string &desc,
-      Information_Model::DataType data_type,
+  std::string addWritableMetric(const std::string& name,
+      const std::string& desc, Information_Model::DataType data_type,
       std::optional<std::function<Information_Model::DataVariant()>> read_cb,
       std::function<void(Information_Model::DataVariant)> write_cb);
 };
