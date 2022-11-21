c55dc3afcb0df6cdd06c1d65fbacf27664845e8a
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2022-11-21 11:26:38 +0100

    simplify integration test

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -13,18 +13,13 @@ int main() {
   try {
     auto repo = std::make_shared<SPD_LoggerRepository>();
     LoggerManager::initialise(repo);
-    auto logger = LoggerManager::registerLogger("main");
-    try {
-      auto technology_manager = ModelManager();
-      if (!technology_manager.getModelEventSource()) {
-        throw runtime_error("Event source can not be null ptr");
-      }
-      logger->info("Integration test succeeded.");
-      exit(EXIT_SUCCESS);
-    } catch (const exception& ex) {
-      logger->error("Integration test failed due to exception: {}", ex.what());
-      exit(EXIT_FAILURE);
+
+    auto technology_manager = ModelManager();
+    if (!technology_manager.getModelEventSource()) {
+      throw runtime_error("Event source can not be null ptr");
     }
+    cout << "Integration test succeeded." << endl;
+    exit(EXIT_SUCCESS);
   } catch (const exception& ex) {
     cerr << "Integration test failed due to exception: " << ex.what() << endl;
     exit(EXIT_FAILURE);
