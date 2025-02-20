5b1b86d8e4dfe021e86cb8b6d42ee6dd04be35ed
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-02-20 12:52:43 +0100

    bump data_consumer_adapter_interface to v0.3, technology_adapter_interface v0.4, remove direct hasll and information_model dependencies

--- conanfile.py
+++ conanfile.py
@@ -54,14 +54,10 @@ class PackageConan(ConanFile):
     def requirements(self):
         # @+ START USER REQUIREMENTS
         self.requires(
-            "data_consumer_adapter_interface/[~0.2]@hahn-schickard/stable", headers=True, transitive_headers=True)
+            "data_consumer_adapter_interface/[~0.3]@hahn-schickard/stable", headers=True, transitive_headers=True)
         self.requires(
-            "technology_adapter_interface/[~0.3]@hahn-schickard/stable", headers=True, transitive_headers=True)
-        self.requires(
-            'information_model/[~0.3]@hahn-schickard/stable', headers=True, transitive_headers=True)
-        self.requires('hasll/[~0.3]@hahn-schickard/stable', headers=True,
-                      libs=True, transitive_headers=True, transitive_libs=True)
-        # @- END USER REQUIREMENTS
+            "technology_adapter_interface/[~0.4]@hahn-schickard/stable", headers=True, transitive_headers=True)
+             # @- END USER REQUIREMENTS
 
     def build_requirements(self):
         self.test_requires("gtest/[~1.11]")
