9a459192717fcdbba2793c7dedf8a5eb342b78d0
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-12-10 14:08:24 +0100

    move getElement into element method

--- sources/Model/GroupImpl.cpp
+++ sources/Model/GroupImpl.cpp
@@ -27,7 +27,42 @@ GroupImpl::ElementVector GroupImpl::asVector() const {
   return result;
 }
 
-ElementPtr GroupImpl::element(const string& ref_id) const {}
+ElementPtr GroupImpl::element(const string& ref_id) const {
+  auto sanitized_id = id_;
+  if (sanitized_id.back() == ':') {
+    sanitized_id.pop_back();
+  }
+  if (ref_id == sanitized_id) {
+    throw IDPointsThisGroup(ref_id);
+  }
+  if (ref_id.compare(0, id_.length(), id_) != 0) {
+    // if referenced id does not start with this id, than the element is not in
+    // this GroupImpl
+    throw ElementNotFound(ref_id);
+  }
+
+  auto sub_id = ref_id.substr(id_.size());
+  if (sub_id.front() == '.') {
+    sub_id.erase(0, 1);
+  }
+  auto group_marker = sub_id.find('.');
+  if (group_marker == string::npos) {
+    if (auto it = elements_.find(sub_id); it != elements_.end()) {
+      return it->second;
+    } else {
+      // no element with given id exists
+      throw ElementNotFound(ref_id);
+    }
+  } else {
+    auto subgroup_id = sub_id.substr(0, group_marker);
+    if (auto it = subgroups_.find(subgroup_id); it != subgroups_.end()) {
+      return it->second->element(ref_id);
+    } else {
+      // no subGroupImpl containing given id exists
+      throw ElementNotFound(ref_id);
+    }
+  }
+}
 
 void GroupImpl::visit(const Visitor& visitor) const {
   for_each(elements_.begin(), elements_.end(), [&visitor](const auto& pair) {
@@ -65,7 +100,7 @@ void GroupImpl::addElement(const ElementPtr& element) {
   }
   auto group_marker = sub_id.find('.');
   if (group_marker != string::npos) {
-    auto parent = getElement(id_ + sub_id.substr(0, group_marker));
+    auto parent = this->element(id_ + sub_id.substr(0, group_marker));
     if (parent->type() != ElementType::Group) {
       throw invalid_argument(
           "Parent element " + parent->id() + " is not a GroupImpl");
@@ -80,46 +115,9 @@ void GroupImpl::addElement(const ElementPtr& element) {
     }
     if (element->type() == ElementType::Group) {
       auto subgroup_id = sub_id.substr(0, sub_id.find('.'));
-      auto subGroupImpl =
+      auto sub_group_impl =
           dynamic_pointer_cast<GroupImpl>(get<GroupPtr>(element->function()));
-      subgroups_.emplace(subgroup_id, subGroupImpl);
-    }
-  }
-}
-
-ElementPtr GroupImpl::getElement(const string& ref_id) {
-  auto sanitized_id = id_;
-  if (sanitized_id.back() == ':') {
-    sanitized_id.pop_back();
-  }
-  if (ref_id == sanitized_id) {
-    throw IDPointsThisGroup(ref_id);
-  }
-  if (ref_id.compare(0, id_.length(), id_) != 0) {
-    // if referenced id does not start with this id, than the element is not in
-    // this GroupImpl
-    throw ElementNotFound(ref_id);
-  }
-
-  auto sub_id = ref_id.substr(id_.size());
-  if (sub_id.front() == '.') {
-    sub_id.erase(0, 1);
-  }
-  auto group_marker = sub_id.find('.');
-  if (group_marker == string::npos) {
-    if (auto it = elements_.find(sub_id); it != elements_.end()) {
-      return it->second;
-    } else {
-      // no element with given id exists
-      throw ElementNotFound(ref_id);
-    }
-  } else {
-    auto subgroup_id = sub_id.substr(0, group_marker);
-    if (auto it = subgroups_.find(subgroup_id); it != subgroups_.end()) {
-      return it->second->element(ref_id);
-    } else {
-      // no subGroupImpl containing given id exists
-      throw ElementNotFound(ref_id);
+      subgroups_.emplace(subgroup_id, sub_group_impl);
     }
   }
 }
